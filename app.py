683ad978c1436b99377a0f0ce5db77e8c4500165
Nihalnsg <nihal.gunathilaka@neosolax.com>
2024-10-07 21:57:24 +0530

    test

--- app.py
+++ app.py
@@ -20,6 +20,7 @@ def predict_car_type(data:CarUser):
     gender=data['gender']
 
     prediction = model.predict([[age, gender]])
+    print('test')
     
     return {
         'prediction': prediction[0]

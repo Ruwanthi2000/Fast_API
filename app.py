dad210c124390a100e4de7785294e571324cc70d
D.M.R.W.Dasanayaka <128591843+Ruwanthi2000@users.noreply.github.com>
2024-10-09 21:29:40 +0530

    app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ model=joblib.load(joblib_in)
 
 @app.get('/')
 def index():
-    return {'message': 'Cars Recommender ML API'}
+    return {'message': 'Cars Recommender ML APIII'}
 
 @app.post('/car/predict')
 def predict_car_type(data:CarUser):
